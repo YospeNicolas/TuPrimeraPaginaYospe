333796c0aa1352247a3550628fd88f6a01fd2404
YospeNicolas <152940040+YospeNicolas@users.noreply.github.com>
2023-12-11 20:04:32 -0300

    Delete proyecto_Yospe directory

--- proyecto_Yospe/webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ingresar_producto
-
-urlpatterns = [
-    path('ingresar-producto/', ingresar_producto, name='ingresar_producto'),
-
-    path('', ingresar_producto, name='raiz'), 
-]
