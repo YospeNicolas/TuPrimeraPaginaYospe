e21f44b99dfd3118d4ae2373cbb290db17cd8655
YospeNicolas <152940040+YospeNicolas@users.noreply.github.com>
2023-12-11 20:04:55 -0300

    Delete proyecto_Yospe3 directory

--- proyecto_Yospe3/proyecto_Yospe/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from webapp.views import ListaProductosView  # Asegúrate de tener la importación correcta
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', ListaProductosView.as_view(), name='lista_productos'),  # Redirige la URL raíz a la lista de productos
-    path('ingresar-producto/', include('webapp.urls')),  # Incluye las URLs de tu aplicación webapp
-]
